e0296672ac55bf9c661ead3c1cfbefb8045c3a07
brenomarinho2001 <74988159+brenomarinho2001@users.noreply.github.com>
2022-07-27 08:42:30 -0300

    aaaa

--- quiguia/src/Components/Pages/Servicos.js
+++ quiguia/src/Components/Pages/Servicos.js
@@ -54,12 +54,7 @@ function Servicos(){
         </div>
 
 
-        <div className="container4">
-            <h2>Igor Teixeira (Ex-aluno)</h2>
-
-            <p>"Aproveite tudo que a vida acadêmica pode te proporcionar e não tenha pressa. Viage. vá a congressos e palestras, faça cursos de verão, veja amostras, seja monitor, vá a grupos de estudos e eventos da universidade. Você conquistou seu lugar aqui, aproveite tudo que lhe é ofertado"</p>
-
-        </div>
+        
 
         <div className="container4">
             <h2>Matulão Bar e Música</h2>
